65aa596036f4b56c0f8db411f16af3ded9aff605
Temuulen Erdenebulgan <erdete01@luther.edu>
2020-03-04 13:21:45 -0600

    add exercise 2

---
+++ exercises/exercise 2/todo_list/js/script.js
@@ -0,0 +1,25 @@
+/* jshint esversion: 6 */
+/* jshint browser: true */
+"use strict;";
+
+var team = ["Aardvark", "Beaver", "Cheetah", "Dolphin", "Elephant", "Flamingo", "Giraffe", "Hippo"];
+var priority = ["Low", "Normal", "Important", "Critical"];
+
+function addTask() {
+}
+
+function addRow(valueList, parent) {
+}
+
+function removeRow() {
+    // 
+}https://stackoverflow.com/questions/26512386/remove-current-row-tr-when-checkbox-is-checked
+
+function populateSelect(selectId, sList) {
+
+}
+
+$(document).ready(function() {
+    populateSelect("#sel_assign", ["Aardvark", "Beaver", "Cheetah", "Dolphin", "Elephant", "Flamingo", "Giraffe", "Hippo"]);
+    populateSelect("#sel_priority", ["Low", "Normal", "Important", "Critical"]);
+});
