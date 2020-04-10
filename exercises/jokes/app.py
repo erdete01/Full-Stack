cd64eaf4379731c70cce9406b150eaf759116f5f
Temuulen Erdenebulgan <erdete01@luther.edu>
2020-04-09 21:28:06 -0500

    added exercise jokes

---
+++ exercises/jokes/app.py
@@ -0,0 +1,30 @@
+# !/usr/bin/env python3
+import random
+import pyjokes
+from flask import Flask, request, render_template, url_for, make_response
+
+app = Flask(__name__)
+@app.route("/", methods=["GET", "POST"])
+
+def index():
+    # Getting all the numbers from form.html when a user enters the number
+    language = request.args.get("language")
+    category = request.args.get("type")
+    number = request.args.get("number")
+
+    # if language and category and number exists, it will run line 15
+    if language and category and number:
+        myNumber = int(number)
+        jokes = send_jokes(language=language, category=category, number=myNumber)
+        numbersOfJokes = str(jokes[0:myNumber]).strip('[]')
+        return render_template("form.html", joke=numbersOfJokes)
+    
+    # When a user opens the page, it will directly call form.html
+    else: 
+        return render_template("form.html")
+
+# generate jokes based on language and category
+def send_jokes(language : str, category : str, number : int):
+    content = pyjokes.get_jokes(language, category)
+    return content
+
