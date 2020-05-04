97afce2467c67de4525357890d7a3ec70d80a81d
erdete01 <44326790+erdete01@users.noreply.github.com>
2020-05-03 22:28:32 -0500

    Add files via upload

---
+++ exercises/login/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, session, redirect, url_for, escape, request, render_template
+import os
+from markupsafe import escape
+
+app = Flask(__name__)
+# app.secret_key = os.environ.get("SECRET_KEY")
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+@app.route("/")
+def index():
+    if 'username' in session:
+        return redirect(url_for('logout'))
+    return redirect(url_for('login'))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('logout'))
+    return render_template("login.html")
+    
+
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    if request.method == 'POST':
+        session.pop('username', None)
+        return redirect(url_for('index'))
+    if 'username' in session:
+        a =  'Hello %s' % escape(session['username'])
+        return render_template("logout.html", name = a)
