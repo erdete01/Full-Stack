2ce957bed84c06555c4a96eccbd9789991ff2719
erdete01 <44326790+erdete01@users.noreply.github.com>
2020-05-13 21:23:08 -0500

    Delete script.js

--- projects/final_project/templates/js/script.js
+++
@@ -1,9 +0,0 @@
-/* jshint esversion: 8 */
-/* jshint node: true */
-/* jshint browser: true */
-/* jshint jquery: true */
-'use strict';
-
-function myFunction() {
-    alert(document.getElementById("roster").rows[0].innerHTML);
-  }
